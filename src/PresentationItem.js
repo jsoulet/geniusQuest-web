3de732e6b5a95d7cab14ed63ed2693b2bc5e6f5a
Johan Soulet <johan.soulet@iadvize.com>
2017-04-12 15:41:27 +0200

    feat (presentation): add bouncing effect

---
+++ src/PresentationItem.js
@@ -0,0 +1,27 @@
+import Avatar from './AvatarGroup/Avatar';
+import React, { Component, PropTypes } from 'react';
+
+class PresentationItem extends Component {
+
+  render (){
+    const {achievement} = this.props;
+
+    return (<div className={`col-xs-4`}>
+      <div className={`PresentationItem bounceIn`} style={{backgroundImage:`url('${achievement.photo}')`}} ref={(e) => {this.domElement = e;}}>
+          <div className="author">
+              <Avatar picture={achievement.userPhoto}/><span className="authorName">{achievement.name}</span>
+          </div>
+      </div>
+    </div>);
+  }
+}
+
+PresentationItem.propTypes = {
+  achievement: PropTypes.shape({
+    photo: PropTypes.string,
+    userPhoto: PropTypes.string,
+    name: PropTypes.string,
+  }),
+};
+
+export default PresentationItem;
--- src/PresentationView.js
+++ src/PresentationView.js
@@ -1,22 +1,57 @@
 import React, { Component, PropTypes } from 'react';
 import _ from 'lodash';
-import Avatar from './AvatarGroup/Avatar';
+import PresentationItem from './PresentationItem';
+
 import './PresentationView.css';
 
 class PresentationView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayedAchievements : _.take(this.props.resolves.achievements, 6),
+      remainingAchievements: _.takeRight(this.props.resolves.achievements, this.props.resolves.achievements.length - 6)
+    };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      2000
+    );
+  }
+
+  tick() {
+    const randomIndex = _.random(this.state.displayedAchievements.length - 1);
+    const newDisplayed = _.map(this.state.displayedAchievements, (achievement, index) => {
+      if (index === randomIndex) {
+        return _.head(this.state.remainingAchievements);
+      }
+      return achievement;
+    });
+    let newRemaining = _.drop(this.state.remainingAchievements);
+    if (this.state.remainingAchievements.length === 1) {
+      newRemaining = this.props.resolves.achievements;
+    }
+    this.setState({
+        displayedAchievements: newDisplayed ,
+        remainingAchievements: newRemaining
+      });
+  }
+
+
   render() {
+      const {displayedAchievements, remainingAchievements} = this.state;
       return (
         <div className="container">
         <div className="PresentationView row">
-          {this.props.resolves.achievements.map((achievement, i) => {
-            return <div className={`col-xs-4`} key={i}>
-            <div className={`PresentationItem`} style={{backgroundImage:`url('${achievement.photo}')`}}>
-                <div className="author">
-                    <Avatar picture={achievement.userPhoto}/><span className="authorName">{achievement.name}</span>
-                </div>
-            </div>
-            </div>
-          })}
+          {displayedAchievements.map((achievement, i) => {
+            if(typeof achievement==='undefined') {
+              console.log({displayedAchievements, remainingAchievements})
+              clearInterval(this.timerID)
+            }
+            return <PresentationItem achievement={achievement} key={i + '-' + achievement.id}/>})}
+        </div>
+        <div className="buffer">
+          {remainingAchievements.map((achievement, i) => <span key={achievement.id} style={{backgroundImage:`url('${achievement.photo}')`}}/>)}
         </div>
         </div>
     );
